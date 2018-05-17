ae12c51ca2830a3a1b2bccd65881062e9abe1b9d
Flask <19504203+D-Flask@users.noreply.github.com>
2018-05-17 21:34:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -17,16 +17,6 @@ app.use(session({
   saveUninitialized: true
 }))
 
-function requiresLogin(req, res, next) {
-  if (req.session && req.session.userId) {
-    return next();
-  } else {
-    var err = new Error('You must be logged in to view this page.');
-    err.status = 401;
-    return next(err);
-  }
-}
-
 app.get('/',function(req,res,next){
 res.sendFile(__dirname + '/index.html');
 });
